3680e8ca13e77c2acaf9a9a55306a9912cbe1f80
bouckita <moun.giscard.gloire.bouckita.ngoma@etu.univ-st-etienne.fr>
2023-01-20 15:13:20 +0100

    initial commit

--- src/Controller/BouckitaController.php
+++ src/Controller/BouckitaController.php
@@ -9,9 +9,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class BouckitaController extends AbstractController
 {
     #[Route('/bouckita', name: 'app_bouckita')]
-    public function index(): Response
+    public function base(): Response
     {
-        return $this->render('bouckita/index.html.twig', [
+        return $this->render('bouckita/base.html.twig', [
+            'controller_name' => 'BouckitaController',
+        ]);
+    }
+    #[Route('/bouckita/cv', name: 'app_bouckita_cv')]
+    Public function cv(): Response 
+    {
+        return $this->render('bouckita/cv.html.twig',[
+            'controller_name' => 'BouckitaController',
+        ]);
+    }
+    #[Route('/bouckita/portfolio', name: 'app_bouckita_portfolio')]
+    Public function portfolio(): Response 
+    {
+        return $this->render('bouckita/portfolio.html.twig',[
+            'controller_name' => 'BouckitaController',
+        ]);
+    }
+    #[Route('/bouckita/formulaire', name: 'app_bouckita_formulaire')]
+    Public function formulaire(): Response 
+    {
+        return $this->render('bouckita/formulaire.html.twig',[
+            'controller_name' => 'BouckitaController',
+        ]);
+    }
+    #[Route('/plus', name: 'app_bouckita_plus')]
+    Public function plus(): Response 
+    {
+        return $this->render('/plus.html.twig',[
             'controller_name' => 'BouckitaController',
         ]);
     }
